7ac7b71bc65ef7e2e16cfdc9fc0ed238ad5040a2
Benedikt Ehinger <science@benediktehinger.de>
2023-02-06 11:28:44 +0100

    increased memory size

--- export.jl
+++ export.jl
@@ -2,6 +2,6 @@
 path_to_dir = abspath("./notebooks_precompute/")
 @info path_to_dir
 using PlutoSliderServer
-PlutoSliderServer.export_directory(path_to_dir, Export_output_dir="./output/", Export_offer_binder=true,Precompute_enabled=true)
+PlutoSliderServer.export_directory(path_to_dir, Export_output_dir="./output/", Export_offer_binder=true,Precompute_enabled=true,Precompute_max_filesize_per_group = 1500000000)
 #PlutoSliderServer.export_directory(path_to_dir,Export_offer_binder=true)
 
